35a4c45728cf2b107af143c6c7dea5aa35b1348e
mifurm <michal.furmankiewicz@hotmail.com>
2023-09-22 22:56:48 +0200

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,1 +1,8 @@
-Get-Date
+$tenant         = "680a6956-d4bd-4da5-a6f4-ca743607f394"
+$ApplicationId  = 'ad94bc49-06c9-4ed7-8554-80163fe9db02'
+$pass           = 'PXl8Q~t5eerjaGcB0ja3FVo3PyA7LxmCnjUDZdpg'
+
+$SecurePassword = ConvertTo-SecureString -String $pass -AsPlainText -Force
+$Credential     = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ApplicationId, $SecurePassword
+
+Connect-AzAccount -ServicePrincipal -TenantId $tenant -Credential $Credential
