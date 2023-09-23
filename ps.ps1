2eabd2334671b3048177cac8a01f4f997b85ae8a
mifurm <michal.furmankiewicz@hotmail.com>
2023-09-23 09:54:23 +0200

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -1,2 +1,18 @@
 #Import-Module -Name Az
+$sub            = "ce5686a3-a664-455d-86cd-0c3518d6920c"
+$vaultName      = "vault-lmtlmpup"
+$storageAcc     = "fssg01"
+$bckpItem       = "fs01"
+
+
 Connect-AzAccount -Identity
+
+Select-AzSubscription -Subscription $sub
+
+$azr=Get-AzRecoveryServicesVault -Name $vaultName
+
+Set-AzRecoveryServicesVaultContext -Vault $azr
+
+$afsContainer = Get-AzRecoveryServicesBackupContainer -FriendlyName $storageAcc -ContainerType AzureStorage
+$afsBkpItem   = Get-AzRecoveryServicesBackupItem -Container $afsContainer -WorkloadType "AzureFiles" -FriendlyName $bckpItem
+$job          = Backup-AzRecoveryServicesBackupItem -Item $afsBkpItem
