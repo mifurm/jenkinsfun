89b4921ac97caa9a6266fff8fa08264af926289f
mifurm <michal.furmankiewicz@hotmail.com>
2023-10-01 21:00:21 +0200

    Update ps.ps1

--- ps.ps1
+++ ps.ps1
@@ -5,9 +5,12 @@ $sub            = "ce5686a3-a664-455d-86cd-0c3518d6920c"
 $vaultName      = "vault-lmtlmpup"
 $storageAcc     = "fssg01"
 $bckpItem       = "fs01"
+$clientId       = '00ad99b4-38cd-43fd-b26a-384d63e92a08'
 
 
 Connect-AzAccount -Identity
+
+Connect-AzAccount -Identity -AccountId $clientId
 
 Select-AzSubscription -Subscription $sub
 
